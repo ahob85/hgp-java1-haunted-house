6eebeb301155a8b3043f52e876c3ed755a0869b8
Aaron Hobson <aaron@ahob85.com>
2021-08-17 12:08:01 -0700

    created assignment

---
+++ HauntedHouse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class HauntedHouse here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HauntedHouse
+{
+
+    public static void main(String[] args) 
+    {
+
+    }
+}
